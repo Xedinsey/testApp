45c0f6960ca18d66b13f76aaf387a679753df4e0
Xedinsey <gameeter@gmail.com>
2021-11-16 22:43:25 +0300

    1

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -3,11 +3,8 @@ package testapp;
 
 public class TestApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Java!");
     }
     
 }
